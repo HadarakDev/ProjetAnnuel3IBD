86cb7186d25fd1350ee69ac4e3e2deca27f1e170
HadarakDev <nico_roche@hotmail.fr>
2019-03-31 22:15:48 +0200

    update linear regression

--- projet/MLAlgorithms/LinearRegression/LinearRegression/LinearRegression.cpp
+++ projet/MLAlgorithms/LinearRegression/LinearRegression/LinearRegression.cpp
@@ -5,46 +5,100 @@
 #include <iostream>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
-
-using namespace cv;
+#include <fstream>
+#include <filesystem>
 using namespace std;
+using namespace cv;
+namespace fs = std::filesystem;
 
+void saveWeight(double *weightArray, double bice)
+{
+	ofstream weightFile;
+	weightFile.open("example.txt");
 
+	weightFile << bice << ";";
+	//for (int i = 0; i < weightArray.)
+	weightFile.close();
+}
 
-int *getArrayFromImage(string imagePath)
+int *getArrayFromImage(string imagePath, bool isGray)
 {
 	Mat image;
-	image = imread(imagePath, 3);
+	int component = 3;
+	if (isGray == true)
+		component = 1;
+	image = imread(imagePath, component);
 	int i = 0;
 	unsigned int imageSize = image.cols * image.rows;
-	int *pixelArray = new int[imageSize * 3]; // a changer pour gray
+	int *pixelArray = new int[imageSize * component]; // a changer pour gray
 
 	for (int x = 0; x < image.rows; x++)
 	{
 		for (int y = 0; y < image.cols; y++)
 		{
 			unsigned char *p = image.ptr(x, y); // order B G R 
-			pixelArray[i] = p[2];
-			pixelArray[i + 1] = p[1];
-			pixelArray[i + 2] = p[0];
-			i = i + 3;
+			for (int c = 0; c < component; c++)
+			{
+				pixelArray[i++] = p[c];
+			}
 		}
 	}
-	for (int k = 0; k < imageSize * 3; k = k + 3)
+	for (int k = 0; k < imageSize * 3; k = k + component)
 	{
-		std::cout << "RED : " << pixelArray[k];
-		std::cout << ", GREEN : " << pixelArray[k + 1] ;
-		std::cout << ", BLUE " << pixelArray[k + 2] << endl;;
-		
+		if (isGray == true)
+			std::cout << "COLOR : " << pixelArray[k] << endl;
+		else
+		{
+			std::cout << "BLUE: " << pixelArray[k];
+			std::cout << ", GREEN : " << pixelArray[k + 1] ;
+			std::cout << ", RED " << pixelArray[k + 2] << endl;
+		}
 	}
 	return (pixelArray);
 }
+
+vector<string> getDirectoryImagesPath(string folderPath)
+{
+	vector<string> imagesPathList;
+	for (auto& p : fs::directory_iterator(folderPath))
+	{
+		string ext = p.path.substr(p.path.find_last_of(".") + 1);
+		if ( ext == "jpg" || ext == "png" || ext == "jpeg")
+		{
+			imagesPathList.push_back(p.path);
+		}
+	}
+	return imagesPathList;
+}
+
+int *getReferenceImageSize(string imagePath)
+{
+	Mat image;
+
+	image = imread(imagePath, component); // 
+	int *imageSize = new int[2];
+	//image = imread(imagePath, 3);
+	imageSize[0] = image.rows;
+	imageSize[1] = image.cols;
+	return imageSize;
+} 
+
 int main()
 {
-	getArrayFromImage("C://Users//nico_//Documents//ConvertedImages//CroppedLotte//0_1_0_0_0.jpg");
+	// arg: train / nom dossier
+	// arg: predict / nom image
+	//open dossier
+	//read files (images)
+
+	string folderPath = "C://Users//nico_//Documents//ConvertedImages//CroppedLotte//";
+
+	vector<string> imagesPath = getDirectoryImagesPath(folderPath);
+	getReferenceImageSize(imagesPath[0]);
+	getArrayFromImage("C://Users//nico_//Documents//ConvertedImages//CroppedLotte//0_1_0_0_0.jpg", false);
     std::cout << "Hello World!\n"; 
 }
+
+// nom de dossier => parcours chaque image (getArrayFromImage) => getWeights/ bice => 
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
