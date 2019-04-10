3e97b49c17e667b04a9e1562fc588d99814cb61d
HadarakDev <nico_roche@hotmail.fr>
2019-04-10 21:21:03 +0200

    fix type on python linear regression :lollipop:

--- projet/MLAlgorithms/python/params.py
+++ projet/MLAlgorithms/python/params.py
@@ -1,7 +1,7 @@
 #Parametre individuel MERCI DE METTRE UN GIT IGNORE SUR CE FICHIER
 
 #Chemin DLL
-pathDLL = "D:/Cours/3IBD/projetAnnuel/projet/MLAlgorithms/ML_Library/x64/Debug/ML_Library.dll"
+pathDLL = "C:\\Users\\nico_\\Documents\\GitHub\\ProjetAnnuel3IBD\\projet\\MLAlgorithms\\ML_Library\\x64\Debug\\ML_Library.dll"
 
 #information dataset
 pathDataset = "D:/Cours/3IBD/projetAnnuel/projet/tmp/train/"
