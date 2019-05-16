c074ed3017139f5b3868f7f40a24882088394969
HadarakDev <32707550+HadarakDev@users.noreply.github.com>
2019-05-16 21:04:38 +0200

    added vector utils and pmc update

--- projet/MLAlgorithms/ML_Library/ML_Library/pmc.cpp
+++ projet/MLAlgorithms/ML_Library/ML_Library/pmc.cpp
@@ -6,21 +6,48 @@ using namespace Eigen;
 
 extern "C" {
 
-	// Initialisation random weight [-1,1]
+	void calculateNeuroneOutput(t_neurone* neurone, Eigen::VectorXd* input);
+	SUPEREXPORT double predictPMCRegression(
+		t_pmc *W,
+		Eigen::VectorXd *X
+	)
+	{
+		try {
+			//for (int idxLayer = 0; idxLayer < W->nbLayer; idxLayer++)
+			for (int idxNeurone = 0; idxNeurone < (*W).layers[0].nbNeurone; idxNeurone++)
+			{
+				calculateNeuroneOutput(&(*W).layers[0].neurones[idxNeurone], X);
+				cout << (*W).layers[0].neurones[idxNeurone].result << endl;
+			}
+		}
+		catch (const std::exception & ex)
+		{
+			std::cout << "Error occurred: " << ex.what() << std::endl;
+			return (-1);
+		}
+		catch (const runtime_error & error)
+		{
+			std::cout << "Error occurred: " << error.what() << std::endl;
+			return (-2);
+		}
+	}
 	SUPEREXPORT void *createPMCModel(int *structure, int nbLayer, int inputCountPerSample)
 	{
 
 		srand(time(NULL));
 		try
-		{
-			t_layer* pmcLayers = NULL;
-			if ((pmcLayers = (t_layer*)malloc(sizeof(t_layer) * nbLayer)) == NULL)
+		{	
+			t_pmc *pmc = NULL;
+			if ((pmc = (t_pmc*)malloc(sizeof(t_pmc) * 1)) == NULL)
+				return (NULL); // if == null throws exception
+			pmc->nbLayer = nbLayer;
+			if ((pmc->layers = (t_layer*)malloc(sizeof(t_layer) * nbLayer)) == NULL)
 				return (NULL); // if == null throws exception
 			for (int idxLayer = 0; idxLayer < nbLayer; idxLayer++)
 			{
-				if ((pmcLayers[idxLayer].neurones = (t_neurone*)malloc(sizeof(t_neurone) * structure[idxLayer])) == NULL)
+				if ((pmc->layers[idxLayer].neurones = (t_neurone*)malloc(sizeof(t_neurone) * structure[idxLayer])) == NULL)
 					return (NULL);
-
+				pmc->layers[idxLayer].nbNeurone = structure[idxLayer];
 				for (int idxNeurone = 0; idxNeurone < structure[idxLayer]; idxNeurone++)
 				{
 					int nbWeights;
@@ -30,19 +57,32 @@ extern "C" {
 						nbWeights = structure[idxLayer - 1];
 					nbWeights++; // ajout du biais
 
-					pmcLayers[idxLayer].neurones[idxNeurone].weights = new Eigen::VectorXd(nbWeights);
+					pmc->layers[idxLayer].neurones[idxNeurone].weights = new Eigen::VectorXd(nbWeights);
 					for (int idxWeights = 0; idxWeights < nbWeights; idxWeights++)
 					{
-						pmcLayers[idxLayer].neurones[idxNeurone].weights(idxWeights) = (rand() / (double)RAND_MAX) * (1.0 - (-1.0)) - 1.0;
+						//(*pmc->layers[idxLayer].neurones[idxNeurone].weights)(idxWeights) = (rand() / (double)RAND_MAX) * (1.0 - (-1.0)) - 1.0;
+						(*pmc->layers[idxLayer].neurones[idxNeurone].weights)(idxWeights) = 1;
 					}
 				}
 			}
-			cout << pmcLayers[2].neurones[1].weights[2] << endl;
-			return (pmcLayers);
+			cout << (*pmc->layers[2].neurones[1].weights)(2) << endl;
+			return (pmc);
 		}
 		catch (const std::exception & ex)
 		{
 			std::cout << "Error occurred: " << ex.what() << std::endl;
 		}
 	}
+}
+
+void calculateNeuroneOutput(t_neurone *neurone, Eigen::VectorXd *input)
+{
+	Eigen::VectorXd tmp(input->size());
+
+	cout << "AA" << endl;
+	tmp = (*input) * (*neurone->weights);
+	(*neurone).result = tmp.sum();
+	cout << tmp.sum() << endl;
+
+
 }
--- projet/MLAlgorithms/ML_Library/ML_Library/pmc.h
+++ projet/MLAlgorithms/ML_Library/ML_Library/pmc.h
@@ -1,12 +1,20 @@
 #include <Eigen/Dense>
 typedef struct s_neurone
 {
+	double		result;
 	Eigen::VectorXd	*weights;
 }				t_neurone;
 
 typedef struct s_layer
 {
-
+	unsigned int nbNeurone;
 	t_neurone	*neurones;
 }				t_layer;
+
+typedef struct s_pmc
+{
+	unsigned int nbLayer;
+	t_layer		*layers;
+}				t_pmc;
+
 
