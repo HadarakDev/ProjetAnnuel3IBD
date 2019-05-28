7c0d67a61a4221c40e6b08086df6685303923bb5
HadarakDev <32707550+HadarakDev@users.noreply.github.com>
2019-05-28 13:58:09 +0200

    update  regression test cases

--- projet/testCase/PMC/regression/linearSimple2D.py
+++ projet/testCase/PMC/regression/linearSimple2D.py
@@ -4,6 +4,7 @@
 from utilsRegression import *
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy.ctypeslib import ndpointer
 
 
 pathDLL = "C:/Users/nico_/Documents/GitHub/ProjetAnnuel3IBD/projet/MLAlgorithms/ML_Library/x64/Release/ML_Library.dll"
@@ -14,24 +15,53 @@ myDll = CDLL(pathDLL)
 #parametre
 alpha = 0.1
 epochs = 10000
-display = 100
+display = int(epochs / 10)
+pmcStruct = [1]
+arrStruct = (c_int * len(pmcStruct))(*pmcStruct)
+c_double_p = POINTER(c_double)
 
-#datas des points a tester
-X = np.array([ [1], [2] ])
-Y = np.array([ 2, 3])
+X = [ 1, 2 ]
+Y = [ 2, 3 ]
+Xnp = np.array([ [1], [2] ])
+Ynp = np.array([ 2, 3])
 
-#recuperer les poids
-pArrayWeight = pmcRegression(myDll, X, Y, alpha, epochs, display, [1, 1])
+myDll = CDLL(pathDLL)
+
+myDll.loadTestCase.argtypes = [POINTER(ARRAY(c_double, len(X))), c_uint, c_uint, c_uint]
+myDll.loadTestCase.restype = c_void_p
+
+pMatrixX = myDll.loadTestCase((c_double * len(X))(*X), 2, 1, 1)
+myDll.loadTestCase.argtypes = [POINTER(ARRAY(c_double, len(Y))), c_uint, c_uint, c_uint]
+pMatrixY = myDll.loadTestCase((c_double * len(Y))(*Y), 2, 1, 0)
+
+myDll.createPMCModel.argtypes = [POINTER(ARRAY(c_int, len(pmcStruct))), c_uint, c_uint]
+myDll.createPMCModel.restype = c_void_p
+
+pArrayWeight = myDll.createPMCModel(arrStruct, len(pmcStruct), 1)
+
+myDll.fitPMCRegression.argtypes = [ c_void_p, c_void_p, c_void_p, c_int, c_int, c_double, c_int, c_int ]
+myDll.fitPMCRegression.restype = c_double								
+error = myDll.fitPMCRegression( pArrayWeight, pMatrixX, pMatrixY, 2, 1, alpha, epochs, display)
+
+myDll.datasetToVector.argtypes = [c_double_p, c_uint, c_uint]
+myDll.datasetToVector.restype = c_void_p
 
 #affichage des points
 X1 = np.linspace(0, 4, 60)
+myDll.predictPMCRegression.argtypes = [c_void_p, c_void_p ]
+myDll.predictPMCRegression.restype = ndpointer(dtype=c_double, shape=(pmcStruct[-1],))
 for x1 in X1:
-   
 	predictX = np.array([x1])
-	value = predict(myDll, myDll.predictPMCRegression, predictX, pArrayWeight)        
-	plt.scatter(x1, value, color='#bbdefb')
-      
+	arr_tmp = (c_double * 1)(*predictX)
+	datasetTmp = myDll.datasetToVector(arr_tmp, len(predictX), 1)
+	value = myDll.predictPMCRegression(pArrayWeight, datasetTmp)
+	print(value[0])   
+	plt.scatter(x1, value[0], color='#bbdefb')
 
+myDll.deletePMC.argtypes = [ c_void_p ]
+myDll.deletePMC(pArrayWeight)
 plt.scatter(X, Y, color='red')
 plt.show()
 plt.clf()
+      
+
