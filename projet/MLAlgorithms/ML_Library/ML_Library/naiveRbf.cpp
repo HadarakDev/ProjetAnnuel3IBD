99732641e73437c8928eb8d3c62beced9a6220f2
El-Tatane <antoine@boudeville.fr>
2019-07-02 21:33:10 +0200

    add load rbf
    pas complètement testé

--- projet/MLAlgorithms/ML_Library/ML_Library/naiveRbf.cpp
+++ projet/MLAlgorithms/ML_Library/ML_Library/naiveRbf.cpp
@@ -1,6 +1,7 @@
 #include "include.h"
 
 using namespace Eigen;
+using namespace std;
 
 extern "C" {
 
@@ -68,5 +69,100 @@ extern "C" {
 	SUPEREXPORT double predictNaiveRBFClassification(t_rbfData* RBF, VectorXd* XPredict)
 	{
 		return predictNaiveRBFRegression(RBF, XPredict) >= 0 ? 1.0 : -1.0;
+	}
+
+	SUPEREXPORT void saveRBFWeightsInCSV(char* path, t_rbfData * RBF)
+	{
+		ofstream fd;
+
+		fd.open(path);
+
+		fd << RBF->X->rows() << ";" << RBF->X->cols();
+
+		// write X
+		for (int x = 0; x < RBF->X->rows(); x++)
+		{
+			for (int y = 0; y < RBF->X->cols() ; y++)
+			{
+				fd << (*RBF->X)(x, y);
+				fd << ";";
+			}
+		}
+		// write W
+		for (int x = 0; x < RBF->W->rows(); x++)
+		{
+			
+			fd << (*RBF->W)(x, 0);
+			fd << ";";
+		}
+		fd.close();
+	}
+	
+	SUPEREXPORT void *loadRBFWeightsWithCSV(char* path, unsigned int inputCountPerSample)
+	{
+		t_rbfData* RBF = new t_rbfData[1];
+		std::string token;
+		unsigned int x;
+		unsigned int y;
+		size_t pos = 0;
+		double value;
+		int XCol;
+		int XRow;
+
+		std::ifstream fd(path);
+
+		if (!fd) {
+			cout << "Unable to open file";
+			exit(1);
+		}
+
+		RBF->inputCountPerSample = inputCountPerSample;
+		
+		//get X size
+		std::string line = "";
+		getline(fd, line);
+
+		pos = line.find(";"); //cou;rty
+		XRow = stod(line.substr(0, pos));
+		line.erase(0, pos + 1);
+		XCol = stod(line.substr(0));
+
+		// get X values
+		RBF->X = new Eigen::MatrixXd(XRow, XCol);
+
+		line = "";
+		x = 0;
+		y = 0;
+		getline(fd, line);
+		while ((pos = line.find(";")) != std::string::npos) {
+			token = line.substr(0, pos);
+			value = stod(token);
+			(*RBF->X)(x, y) = value;
+			line.erase(0, pos + 1);
+			y++;
+
+			if (y >= XCol) {
+				y = 0;
+				x++;
+			}
+		}
+
+		// get W values
+		RBF->W = new Eigen::MatrixXd(inputCountPerSample, 1);
+
+		x = 0;
+		line = "";
+		getline(fd, line);
+		while ((pos = line.find(";")) != std::string::npos) {
+			token = line.substr(0, pos);
+			value = stod(token);
+			(*RBF->W)(x, 0) = value;
+			line.erase(0, pos + 1);
+			x++;
+		}
+
+		
+		return (RBF);
+		
 	}
 }
